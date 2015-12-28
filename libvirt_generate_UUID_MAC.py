d0211718bb1dabddca775767a2c9ebbd32a8a2f6
Savitoj <savitojs@gmail.com>
2015-12-28 21:09:34 +0530

    Script to generate UUID and unique MAC addr

---
+++ libvirt_generate_UUID_MAC.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#  -*- mode: python; -*-
+print ""
+print "New UUID:"
+import virtinst.util ; print virtinst.util.uuidToString(virtinst.util.randomUUID())
+print "New MAC:"
+import virtinst.util ; print virtinst.util.randomMAC()
+print ""
